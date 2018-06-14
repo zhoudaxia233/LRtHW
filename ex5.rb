f4c9a82929143699cbafdaf462fb27d6d8ac0393
zheng <yootaoo@gmail.com>
2018-06-14 11:30:57 +0200

    ex5

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Zheng Zhou'
+my_age = 25
+my_height = 68
+my_weight = 117
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too thin."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
